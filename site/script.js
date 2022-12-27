a7fde5ccab37c6c8fb9b04a1144ec7a4970074d9
chris <chrishayes042@gmail.com>
2022-12-26 21:06:57 -0700

    updated comments

--- site/script.js
+++ site/script.js
@@ -3,20 +3,22 @@ const unTextField = document.getElementById("userNameText");
 const loginButton = document.querySelector("#login");
 
 // console.log(nameText);
-
+// Console logs the name inside the textfield
 function logName() {
   let nameText = document.querySelector("#userNameText").value;
-  console.log(errorCheck(nameText));
+  // check for errors
   if (errorCheck(nameText) == true) {
+    // resets the css for the textfield
     unTextField.style = "userNameText";
+
     console.log(nameText);
   }
 }
+// checks the length of the parameter
 function errorCheck(name) {
   if (name.length == 0) {
     document.getElementById("error").innerHTML = "Please input a user name";
     unTextField.style.border = "2px solid red";
-
     return false;
   } else {
     return true;
