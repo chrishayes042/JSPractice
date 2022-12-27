88668445dec4fa82f90e08a0a823cb7cbd611ff5
chris <chrishayes042@gmail.com>
2022-12-26 21:59:20 -0700

    Added new method and operations

--- site/script.js
+++ site/script.js
@@ -1,28 +1,32 @@
 let element = document.querySelector("div1");
-const unTextField = document.getElementById("userNameText");
+const unTextField = document.querySelector("#userNameText");
+const errorMsg = document.querySelector("#error");
 const loginButton = document.querySelector("#login");
 
 // console.log(nameText);
 // Console logs the name inside the textfield
 function logName() {
+  // Get value from the text box
   let nameText = document.querySelector("#userNameText").value;
-  // check for errors
-  if (errorCheck(nameText) == true) {
-    // resets the css for the textfield
-    unTextField.style = "userNameText";
-
-    console.log(nameText);
-  }
+  // check for errors, uses a ternary operator
+  errorCheck(nameText)
+    ? (clearErrors(), console.log(nameText))
+    : console.log("Error");
 }
 // checks the length of the parameter
 function errorCheck(name) {
   if (name.length == 0) {
-    document.getElementById("error").innerHTML = "Please input a user name";
+    errorMsg.innerHTML = "Please input a user name";
     unTextField.style.border = "2px solid red";
     return false;
   } else {
     return true;
   }
+}
+// func to clear the errors and set the css back to original
+function clearErrors() {
+  errorMsg.innerHTML = "";
+  unTextField.style = "userNameText";
 }
 
 // function load() {
